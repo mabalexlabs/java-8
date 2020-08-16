c2ad97ad034b91f126ab800a11e8d245b27a28f0
amiyakumar.m <amiyakumar.m@wlpt753.imidomain.com>
2020-08-16 20:21:46 +0530

    Functional Interface in Util Package...

--- java8-functional-interface/src/Demo.java
+++ java8-functional-interface/src/Demo.java
@@ -1,3 +1,9 @@
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 @FunctionalInterface
 interface Square {
@@ -9,6 +15,24 @@ public class Demo {
 	public static void main(String[] args) {
 		Square square = (int x) -> x * x;
 		System.out.println(square.calculate(5));
+
+		Predicate<String> predicate = name -> name.startsWith("temp");
+		System.out.println(predicate.test("tempVariable"));
+
+		Function<String, Integer> function = name -> name.length();
+		System.out.println(function.apply("temp"));
+
+		UnaryOperator<String> unaryOperator = name -> name + "Varaible";
+		System.out.println(unaryOperator.apply("temp"));
+
+		BinaryOperator<String> binaryOperator = (a, b) -> a + b;
+		System.out.println(binaryOperator.apply("temp", "Variable"));
+
+		Consumer<String> consumer = System.out::println;
+		consumer.accept("demo");
+
+		Supplier<Integer> supplier = () -> 50;
+		System.out.println(supplier.get());
 	}
 
 }
